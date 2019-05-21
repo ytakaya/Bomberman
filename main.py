9799e35ba60b1766b54e58ecb34d63889362dc6c
takaya <mt.tky@icloud.com>
2019-05-21 23:17:36 +0900

    make field

--- main.py
+++ main.py
@@ -2,8 +2,26 @@ from pyglet.gl import *
 from pyglet.graphics import TextureGroup
 from pyglet.window import key, mouse
 
+class Window(pyglet.window.Window):
+    def __init__(self, *args, **kwargs):
+        super(Window, self).__init__(*args, **kwargs)
+
 def main():
-    window = pyglet.window.Window(800,600,caption='Pyglet')
+    window = pyglet.window.Window(640,640,caption='Pyglet')
+    quads = []
+    for i in range(7):
+        quad = pyglet.graphics.vertex_list(2,
+            ('v2f', [0,i*80+80,800,i*80+80]),
+            ('c3B', (255,255,255,255,255,255)))
+        quads.append(quad)
+        quad = pyglet.graphics.vertex_list(2,
+            ('v2f', [i*80+80,0,i*80+80,800]),
+            ('c3B', (255,255,255,255,255,255)))
+        quads.append(quad)
+    @window.event
+    def on_draw():
+        for quad in quads:
+            quad.draw(pyglet.gl.GL_LINES)
     pyglet.app.run()
 
 if __name__=='__main__':
