5b45a90d893e93704d3b4ec23da07a8ea1684c7d
takaya <mt.tky@icloud.com>
2019-05-22 10:52:59 +0900

    add player

--- main.py
+++ main.py
@@ -2,26 +2,55 @@ from pyglet.gl import *
 from pyglet.graphics import TextureGroup
 from pyglet.window import key, mouse
 
+TEXTURE_PATH = 'player.png'
+PLAYER = pyglet.image.load(TEXTURE_PATH)
+
+class Model(object):
+
+    def __init__(self):
+        self.world = {}
+        self.player = {}
+        self.quads = []
+
+        self._initialize()
+
+    def _initialize(self):
+        """
+        Initialize the field.
+        """
+        for i in range(12):
+            quad = pyglet.graphics.vertex_list(2,
+                ('v2f', [0,i*50+50,600,i*50+50]),
+                ('c3B', (255,255,255,255,255,255)))
+            self.quads.append(quad)
+            quad = pyglet.graphics.vertex_list(2,
+                ('v2f', [i*50+50,0,i*50+50,600]),
+                ('c3B', (255,255,255,255,255,255)))
+            self.quads.append(quad)
+
+    def show_player(self, player_num):
+        position = self.player[player_num]
+        self._show_player(position)
+
+    def _show_player(self, position):
+        x, y = position
+
+
 class Window(pyglet.window.Window):
     def __init__(self, *args, **kwargs):
         super(Window, self).__init__(*args, **kwargs)
+        self.model = Model()
+
+    def on_draw(self):
+        for quad in self.model.quads:
+            quad.draw(pyglet.gl.GL_LINES)
+        sprite = pyglet.sprite.Sprite(img=PLAYER)
+        sprite.scale = 50/sprite.width
+        sprite.draw()
 
 def main():
-    window = pyglet.window.Window(640,640,caption='Pyglet')
+    window = Window(600,600,caption='Pyglet')
     quads = []
-    for i in range(7):
-        quad = pyglet.graphics.vertex_list(2,
-            ('v2f', [0,i*80+80,800,i*80+80]),
-            ('c3B', (255,255,255,255,255,255)))
-        quads.append(quad)
-        quad = pyglet.graphics.vertex_list(2,
-            ('v2f', [i*80+80,0,i*80+80,800]),
-            ('c3B', (255,255,255,255,255,255)))
-        quads.append(quad)
-    @window.event
-    def on_draw():
-        for quad in quads:
-            quad.draw(pyglet.gl.GL_LINES)
     pyglet.app.run()
 
 if __name__=='__main__':
