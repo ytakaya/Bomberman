cb8e34e88419c4261b06b29414fd3053de7924f5
takaya <mt.tky@icloud.com>
2019-05-24 23:35:10 +0900

    add hit() fuction

--- main.py
+++ main.py
@@ -41,23 +41,63 @@ class Window(pyglet.window.Window):
         self.player = []
         self.player.append(pyglet.sprite.Sprite(img=PLAYER,batch=self.batch))
         self.player[0].scale = 50/self.player[0].width
+        self.player.append(pyglet.sprite.Sprite(img=PLAYER,x=self.width-50,y=self.height-50,batch=self.batch))
+        self.player[1].scale = 50/self.player[1].width
         self.bomb = deque([])
         self.explosion = deque([])
 
         pyglet.clock.schedule_interval(self.update, 1.0 / TICKS_PER_SEC)
 
+    def hit(self,exs,player):
+        """If bomb hit the player?
+
+        Parameters
+        ----------
+        exs : pyglet.sprite.Sprite
+            Sprite object of explosion
+        player : pyglet.sprite.Sprite
+            Sprite object of player
+
+        Returns
+        -------
+        If hit : bool
+        """
+        pl_x,pl_y = player.x,player.y
+        for ex in exs:
+            ex_x,ex_y = ex.x,ex.y
+            if ex_x==pl_x and ex_y==pl_y:
+                return True
+        return False
+
     def on_key_press(self, symbol, modifiers):
-        if symbol==key.RIGHT and self.player[0].x<self.width-50:
+        # player0
+        if symbol==key.D and self.player[0].x<self.width-50:
             self.player[0].x += 50
-        if symbol==key.UP and self.player[0].y<self.height-50:
+        if symbol==key.W and self.player[0].y<self.height-50:
             self.player[0].y += 50
-        if symbol==key.LEFT and self.player[0].x>0:
+        if symbol==key.A and self.player[0].x>0:
             self.player[0].x -= 50
-        if symbol==key.DOWN and self.player[0].y>0:
+        if symbol==key.S and self.player[0].y>0:
             self.player[0].y -= 50
-        if symbol==key.W:
+        if symbol==key.V:
             x = self.player[0].x
             y = self.player[0].y
+            b = pyglet.sprite.Sprite(img=BOMB,x=x,y=y,batch=self.batch)
+            b.scale = 50/b.width
+            self.bomb.append([b,0])
+
+        # player1
+        if symbol==key.RIGHT and self.player[1].x<self.width-50:
+            self.player[1].x += 50
+        if symbol==key.UP and self.player[1].y<self.height-50:
+            self.player[1].y += 50
+        if symbol==key.LEFT and self.player[1].x>0:
+            self.player[1].x -= 50
+        if symbol==key.DOWN and self.player[1].y>0:
+            self.player[1].y -= 50
+        if symbol==key.M:
+            x = self.player[1].x
+            y = self.player[1].y
             b = pyglet.sprite.Sprite(img=BOMB,x=x,y=y,batch=self.batch)
             b.scale = 50/b.width
             self.bomb.append([b,0])
@@ -74,12 +114,15 @@ class Window(pyglet.window.Window):
             new_bomb = deque()
             while self.bomb:
                 b = self.bomb.popleft()
+                # Bomb explode on 1s after put
                 if b[1]>1:
                     explosions = []
-                    explosions.append(pyglet.sprite.Sprite(img=EXPLOSION,x=b[0].x,y=b[0].y,batch=self.batch))
-                    explosions.append(pyglet.sprite.Sprite(img=EXPLOSION,x=b[0].x+50,y=b[0].y,batch=self.batch))
-                    explosions[0].scale = 50/explosions[0].width
-                    explosions[1].scale = 50/explosions[1].width
+                    # Explosion occur at all sides of bomb
+                    dxy = [(-50,0),(0,0),(50,0),(0,-50),(0,50)]
+                    for dx,dy in dxy:
+                        explosions.append(pyglet.sprite.Sprite(
+                            img=EXPLOSION,x=b[0].x+dx,y=b[0].y+dy,batch=self.batch))
+                        explosions[-1].scale = 50/explosions[-1].width
                     self.explosion.append([explosions,0])
                 else:
                     b[1] += dt
@@ -90,10 +133,19 @@ class Window(pyglet.window.Window):
             new_ex = deque()
             while self.explosion:
                 ex, t = self.explosion.popleft()
+                for i,pl in enumerate(self.player):
+                    if self.hit(ex,pl):
+                        print('Player{} BOMB!!'.format(i+1))
+                        print('Winner : Player{}'.format((i+1)%2+1))
+                        print('Looser : Player{}'.format((i+1)))
+                        self.on_window_close()
                 if t<0.2:
                     t += dt
                     new_ex.append([ex,t])
             self.explosion = new_ex
+
+    def on_window_close(self):
+        pyglet.app.exit()
 
 def main():
     window = Window(600,600,caption='Pyglet')
